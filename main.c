7fed2372d5c630d42a5419bf4c1c68f15de69787
DESKTOP-U0D2UTH\theni <thenick113@163.com>
2017-05-22 16:52:27 -0700

    fix bugs and tested under the os-class server

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+//Basil Akzamil
+
 /*
  * A sample, extra-simple block driver. Updated for kernel 2.6.31.
  *
@@ -18,9 +20,20 @@
 #include <linux/genhd.h>
 #include <linux/blkdev.h>
 #include <linux/hdreg.h>
+#include <linux/crypto.h> //Linux Kernel Crypto API
 
 MODULE_LICENSE("Dual BSD/GPL");
 static char *Version = "1.4";
+
+//Represent the key to be passed in during insmod
+//insmod sbd key = "secret"
+//This value should be hashed during initialization
+static char *key = "default";
+
+//charp: A char ointer value. Memory is allocated for user-provided strings,
+//and the pointer is set accordingly
+//S_IRUGO: Used for a parameter that can be read by the world but cannot be changed
+module_param(key, charp, S_IRUGO);
 
 static int major_num = 0;
 module_param(major_num, int, 0);
@@ -28,6 +41,11 @@ static int logical_block_size = 512;
 module_param(logical_block_size, int, 0);
 static int nsectors = 1024; /* How big the drive is */
 module_param(nsectors, int, 0);
+
+//Crypto Variables:
+struct crypto_cipher *tfm;
+static char * stored_key = "CS444Projec3";
+static int validKey = 1;
 
 /*
  * We can tweak our hardware sector size, but the kernel talks to us
@@ -44,11 +62,21 @@ static struct request_queue *Queue;
  * The internal representation of our device.
  */
 static struct sbd_device {
-	unsigned long size;
-	spinlock_t lock;
-	u8 *data;
-	struct gendisk *gd;
+    unsigned long size;
+    spinlock_t lock;
+    u8 *data;
+    struct gendisk *gd;
 } Device;
+
+
+//verbose purposes
+void printk_hex(u8 * hex, int length) {
+    int i;
+    for (i = 0 ; i < length ; ++i) {
+        printk("%02x", hex[i]);
+    }
+    printk("\n");
+}
 
 /*
  * Handle an I/O request.
@@ -113,26 +141,25 @@ static void sbd_transfer(struct sbd_device *dev, sector_t sector,
 
 }
 
-
 static void sbd_request(struct request_queue *q) {
-	struct request *req;
+    struct request *req;
 
-	req = blk_fetch_request(q);
-	while (req != NULL) {
-		// blk_fs_request() was removed in 2.6.36 - many thanks to
-		// Christian Paro for the heads up and fix...
-		//if (!blk_fs_request(req)) {
-		if (req == NULL || (req->cmd_type != REQ_TYPE_FS)) {
-			printk (KERN_NOTICE "Skip non-CMD request\n");
-			__blk_end_request_all(req, -EIO);
-			continue;
-		}
-		sbd_transfer(&Device, blk_rq_pos(req), blk_rq_cur_sectors(req),
-				req->buffer, rq_data_dir(req));
-		if ( ! __blk_end_request_cur(req, 0) ) {
-			req = blk_fetch_request(q);
-		}
-	}
+    req = blk_fetch_request(q);
+    while (req != NULL) {
+        // blk_fs_request() was removed in 2.6.36 - many thanks to
+        // Christian Paro for the heads up and fix...
+        //if (!blk_fs_request(req)) {
+        if (req == NULL || (req->cmd_type != REQ_TYPE_FS)) {
+            printk (KERN_NOTICE "Skip non-CMD request\n");
+            __blk_end_request_all(req, -EIO);
+            continue;
+        }
+        sbd_transfer(&Device, blk_rq_pos(req), blk_rq_cur_sectors(req),
+                bio_data(req->bio), rq_data_dir(req));
+        if ( ! __blk_end_request_cur(req, 0) ) {
+            req = blk_fetch_request(q);
+        }
+    }
 }
 
 /*
@@ -141,80 +168,100 @@ static void sbd_request(struct request_queue *q) {
  * use tools such as fdisk to partition the drive otherwise.
  */
 int sbd_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
-	long size;
+    long size;
 
-	/* We have no real geometry, of course, so make something up. */
-	size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
-	geo->cylinders = (size & ~0x3f) >> 6;
-	geo->heads = 4;
-	geo->sectors = 16;
-	geo->start = 0;
-	return 0;
+    /* We have no real geometry, of course, so make something up. */
+    size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
+    geo->cylinders = (size & ~0x3f) >> 6;
+    geo->heads = 4;
+    geo->sectors = 16;
+    geo->start = 0;
+    return 0;
 }
 
 /*
  * The device operations structure.
  */
 static struct block_device_operations sbd_ops = {
-		.owner  = THIS_MODULE,
-		.getgeo = sbd_getgeo
+        .owner  = THIS_MODULE,
+        .getgeo = sbd_getgeo
 };
 
 static int __init sbd_init(void) {
-	/*
-	 * Set up our internal device.
-	 */
-	Device.size = nsectors * logical_block_size;
-	spin_lock_init(&Device.lock);
-	Device.data = vmalloc(Device.size);
-	if (Device.data == NULL)
-		return -ENOMEM;
-	/*
-	 * Get a request queue.
-	 */
-	Queue = blk_init_queue(sbd_request, &Device.lock);
-	if (Queue == NULL)
-		goto out;
-	blk_queue_logical_block_size(Queue, logical_block_size);
-	/*
-	 * Get registered.
-	 */
-	major_num = register_blkdev(major_num, "sbd");
-	if (major_num < 0) {
-		printk(KERN_WARNING "sbd: unable to get major number\n");
-		goto out;
-	}
-	/*
-	 * And the gendisk structure.
-	 */
-	Device.gd = alloc_disk(16);
-	if (!Device.gd)
-		goto out_unregister;
-	Device.gd->major = major_num;
-	Device.gd->first_minor = 0;
-	Device.gd->fops = &sbd_ops;
-	Device.gd->private_data = &Device;
-	strcpy(Device.gd->disk_name, "sbd0");
-	set_capacity(Device.gd, nsectors);
-	Device.gd->queue = Queue;
-	add_disk(Device.gd);
+    //Check key against stored_key
 
-	return 0;
+    
+    if(key==NULL || strlen(key) == 0){
+        key = stored_key;
+        printk(KERN_WARNING "No vaild key - using stored_key");
+    }
+    
+
+    /*
+     * Set up our internal device.
+     */
+    Device.size = nsectors * logical_block_size;
+    spin_lock_init(&Device.lock);
+    Device.data = vmalloc(Device.size);
+    if (Device.data == NULL)
+        return -ENOMEM;
+    /*
+     * Get a request queue.
+     */
+    Queue = blk_init_queue(sbd_request, &Device.lock);
+    if (Queue == NULL)
+        goto out;
+    blk_queue_logical_block_size(Queue, logical_block_size);
+    /*
+     * Get registered.
+     */
+    major_num = register_blkdev(major_num, "sbd");
+    if (major_num < 0) {
+        printk(KERN_WARNING "sbd: unable to get major number\n");
+        goto out;
+    }
+
+    // Allocating Crypto API for AES cipher
+    tfm = crypto_alloc_cipher("aes", 0, 0);
+    if (tfm == 0) {
+        printk(KERN_WARNING "sbd_enc: ERROR: unable to allocate tfm\n");
+        goto out;
+    }
+    printk("sbd: KEY = %s\n", key);
+    crypto_cipher_setkey(tfm, stored_key, strlen(stored_key));
+
+    /*
+     * And the gendisk structure.
+     */
+    Device.gd = alloc_disk(16);
+    if (!Device.gd)
+        goto out_unregister;
+    Device.gd->major = major_num;
+    Device.gd->first_minor = 0;
+    Device.gd->fops = &sbd_ops;
+    Device.gd->private_data = &Device;
+    strcpy(Device.gd->disk_name, "sbd0");
+    set_capacity(Device.gd, nsectors);
+    Device.gd->queue = Queue;
+    add_disk(Device.gd);
+
+    return 0;
 
 out_unregister:
-	unregister_blkdev(major_num, "sbd");
+    unregister_blkdev(major_num, "sbd");
 out:
-	vfree(Device.data);
-	return -ENOMEM;
+    vfree(Device.data);
+    return -ENOMEM;
 }
 
 static void __exit sbd_exit(void)
 {
-	del_gendisk(Device.gd);
-	put_disk(Device.gd);
-	unregister_blkdev(major_num, "sbd");
-	blk_cleanup_queue(Queue);
-	vfree(Device.data);
+    crypto_free_cipher(tfm); //freeing crypto key
+    del_gendisk(Device.gd);
+    put_disk(Device.gd);
+    unregister_blkdev(major_num, "sbd");
+    blk_cleanup_queue(Queue);
+    vfree(Device.data);
 }
 
 module_init(sbd_init);
